bc4fd2bcbd5d27585f60f3cc61ccd50f5f877cf8
Gonçalo Machado <goncalofmachado01@gmail.com>
2023-04-06 19:40:38 +0100

    Wa to download

--- WSProject1/WSProject1/settings.py
+++ WSProject1/WSProject1/settings.py
@@ -126,6 +126,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 INSTALLED_APPS = (
     # ...
+    'app.apps.AppConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4'
     # ...
 )
